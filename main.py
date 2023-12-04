0c2e7315616096852b0959d5f074e93fcbfeb780
Niedzkt <mikolaj.lasota.kor@gmail.com>
2023-12-04 12:58:14 +0100

    added scroll and move mouse for better solution

--- main.py
+++ main.py
@@ -19,32 +19,69 @@ def locateOnScreen(imagePath, confidence=0.8):
         return None
     return max_loc
 
-def isDiscordActive(windowTitle = 'Discord'):
+def isDiscordActive(windowTitle='Discord'):
     try:
         activeWindow = gw.getActiveWindow()
         return windowTitle.lower() in activeWindow.title.lower()
     
     except:
         return False
+    
+def moveMouseUp(start_pos, distance=10, step=1):
+    x, y = start_pos
+    while y > 0:
+        new_y = y - step
+        pyautogui.moveTo(x, new_y)
+        time.sleep(0.01) 
+        y = new_y
+        if y <= start_pos[1] - distance:
+            break
 
+def slowScrollUp(scroll_amount=100, step=10):
+    for _ in range(0, scroll_amount, step):
+        pyautogui.scroll(step) 
+        time.sleep(0.1)
+        
+        
 binImage = 'img/bin.png'
+menuImage = 'img/menu.png'
+confirmImage = 'img/confirm.png'
 
 while True:
     if isDiscordActive():
-        binImageLocation = locateOnScreen(binImage)
-        if binImageLocation is not None:
-            pyautogui.keyDown('shift')
-            pyautogui.click(binImageLocation)
-            pyautogui.keyUp('shift')
+        dotsImageLocation = locateOnScreen(menuImage)
+        if dotsImageLocation is not None:
+            pyautogui.click(dotsImageLocation)
             time.sleep(1)
-            
-            pyautogui.press('up')
+
+            binImageLocation = locateOnScreen(binImage)
+            if binImageLocation is not None:
+                pyautogui.click(binImageLocation)
+                time.sleep(1)
+                print("Message deleted")
+                
+                confirmImageLocation = locateOnScreen(confirmImage)
+                if confirmImageLocation is not None:
+                    pyautogui.click(confirmImageLocation)
+                    print("Confirmation clicked")
+                else:
+                    print("Confirmation not found")
+                    
+            else:
+                pyautogui.click(dotsImageLocation)
+                print("Bin not found")
+
+            pyautogui.press('down')
             time.sleep(1)
-            pass
-        
+        else:
+            print("Dots not found. Moving mouse up...")
+            current_mouse_pos = pyautogui.position()
+            slowScrollUp(scroll_amount=1000, step=100)
+            moveMouseUp(current_mouse_pos, distance=100, step=5)
+            time.sleep(1) 
+    
     else:
         print("Discord is not active. Waiting...")
         time.sleep(5)
-        print("Bin not found")
-    
+
     time.sleep(1)
